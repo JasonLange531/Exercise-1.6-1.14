c13382320afb67f0c51cdcbed814245ff47da062
JasonLange531 <jasey187@gmail.com>
2026-04-01 11:41:23 +0200

    corrected formating and removed redundant files

--- Exercise 1.6-1.14/src/App.jsx
+++ Exercise 1.6-1.14/src/App.jsx
@@ -8,14 +8,14 @@ const App = () => {
 
   return (
     <div>
-      <h1>give feedback</h1>
+      <h1>Give Feedback</h1>
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h1>statistics</h1> 
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>    
+      <h1>Statistics</h1> 
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>    
     </div>
   )
 }
