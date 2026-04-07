cc0d958c257aa9c3d467a38d6e197cba519a79f9
JasonLange531 <jasey187@gmail.com>
2026-04-07 07:48:14 +0200

    chore:only display statistics once feedback has been gathered

--- Exercise 1.6-1.14/src/Statistics.jsx
+++ Exercise 1.6-1.14/src/Statistics.jsx
@@ -1,15 +1,24 @@
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad
 
+  if (all === 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h1>Statistics</h1>
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
-      <p>All {good + neutral + bad}</p>
-      <p>Average {(good - bad) / (good + neutral + bad)}</p>
-      <p>Positive {good / (good + neutral + bad) * 100} %</p>
+      <p>All {all}</p>
+      <p>Average {(good - bad) / all}</p>
+      <p>Positive {(good / all) * 100} %</p>
     </div>
   )
 }
